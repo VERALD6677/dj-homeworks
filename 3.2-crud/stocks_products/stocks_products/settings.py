2d8ddf2337b7d0dd15b30f6394529ed972c7fcd1
root <root@cv4370141.novalocal>
2024-12-25 22:49:20 +0300

    Remove unnecessary files and resolve conflicts

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,6 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
+        'USER': 'postgres',  # The user created without a password
+        'PASSWORD': '1',  # Leave empty if no password is set
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
